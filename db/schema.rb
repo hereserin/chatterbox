b86b1836aef47bf758b496a46c34efdf93e0384e
Erin Marshall <erin.e.mars@gmail.com>
2019-02-22 16:26:55 -0500

    Change cable settings for production

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_09_221113) do
+ActiveRecord::Schema.define(version: 2019_02_12_040754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "chat_memberships", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "chat_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "chat_messages", force: :cascade do |t|
     t.text "content"
@@ -33,14 +26,6 @@ ActiveRecord::Schema.define(version: 2019_02_09_221113) do
   create_table "chats", force: :cascade do |t|
     t.string "chatroom_name"
     t.integer "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "chat_id"
-    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
